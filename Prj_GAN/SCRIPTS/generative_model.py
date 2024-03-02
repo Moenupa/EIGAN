9eec993d8a1bdceb6e56f983db00fe7ddc57bff4
Moenupa WANG <moenupa@outlook.com>
2024-03-02 15:42:04 -0500

    update

--- Prj_GAN/SCRIPTS/generative_model.py
+++ Prj_GAN/SCRIPTS/generative_model.py
@@ -19,7 +19,7 @@ import wandb
 import matplotlib
 from scipy.stats import wasserstein_distance as EMD
 
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 
 
 class WGAN_SIMPLE(nn.Module):
@@ -47,41 +47,40 @@ class WGAN_SIMPLE(nn.Module):
         self.nlatent = nlatent
         self.device = device
         self.scaler = None
-        
+
         self.gen = nn.Sequential(
-            nn.Linear(self.nlatent,nhid),
+            nn.Linear(self.nlatent, nhid),
             nn.LeakyReLU(0.1),
             nn.Dropout(0.4),
-            nn.Linear(nhid,nhid),
+            nn.Linear(nhid, nhid),
             nn.LeakyReLU(0.1),
             nn.Dropout(0.4),
-            nn.Linear(nhid,nhid),
+            nn.Linear(nhid, nhid),
             nn.LeakyReLU(0.1),
             nn.Dropout(0.4),
-            nn.Linear(nhid,ndim),
+            nn.Linear(nhid, ndim),
         )
-        
+
         self.disc = nn.Sequential(
-            nn.Linear(self.ndim,nhid),
+            nn.Linear(self.ndim, nhid),
             nn.LeakyReLU(0.1),
-            nn.Linear(nhid,nhid),
+            nn.Linear(nhid, nhid),
             nn.LeakyReLU(0.1),
-            nn.Linear(nhid,nhid),
+            nn.Linear(nhid, nhid),
             nn.LeakyReLU(0.1),
-            nn.Linear(nhid,1),
+            nn.Linear(nhid, 1),
         )
-        
+
         self.gen.apply(init_weights)
         self.disc.apply(init_weights)
-        
+
         self.gen.to(device)
         self.disc.to(device)
-    
-    def normalize(self,data):
+
+    def normalize(self, data):
         self.scaler = MinMaxScaler()
         self.scaler.fit(data)
         return self.scaler.transform(data)
-            
 
     def optimize(self, data, output_path, batch_size=128, use_wandb=False, lr=1e-4,
                  beta1=0.5, lambda_term=10, epochs=200, kkd=1, kkg=1, device="cpu"):
@@ -93,16 +92,17 @@ class WGAN_SIMPLE(nn.Module):
         dataloader = DataLoader(map_dataset, batch_size=batch_size,
                                 shuffle=True, num_workers=1)
 
-        ## Your Code Goes Here
-
-        ## Your Code Ends Here
+        # Your Code Goes Here
+        
+        # Your Code Ends Here
 
     def calculate_gradient_penalty(self, real_images, fake_images, lambda_term):
         batch_size = real_images.shape[0]
         eta = torch.FloatTensor(batch_size, 1).uniform_(0, 1)
         eta = eta.expand(batch_size, real_images.size(1)).to(self.device)
 
-        interpolated = eta * real_images + ((1 - eta) * fake_images).to(self.device)
+        interpolated = eta * real_images + \
+            ((1 - eta) * fake_images).to(self.device)
 
         # define it to calculate gradient
         interpolated = Variable(interpolated, requires_grad=True)
@@ -112,64 +112,64 @@ class WGAN_SIMPLE(nn.Module):
 
         # calculate gradients of probabilities with respect to examples
         gradients = autograd.grad(outputs=prob_interpolated, inputs=interpolated,
-                                  grad_outputs=torch.ones(prob_interpolated.size()).to(self.device),
+                                  grad_outputs=torch.ones(
+                                      prob_interpolated.size()).to(self.device),
                                   create_graph=True, retain_graph=True)[0]
-        grad_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * lambda_term
+        grad_penalty = ((gradients.norm(2, dim=1) - 1)
+                        ** 2).mean() * lambda_term
         return grad_penalty
 
-    def load(self,checkpoint):
+    def load(self, checkpoint):
         self.load_state_dict(checkpoint["model_state_dict"])
         self.scaler = checkpoint["scaler"]
-    
-    def generate(self,num=50000):
-        fake_data = np.zeros((num,self.ndim))
+
+    def generate(self, num=50000) -> np.ndarray:
+        fake_data = np.zeros((num, self.ndim))
         # if num is divisible by 100, generate by batch, else generate one by one
-        if num % 100 ==0:
-            for i in range(int(num/100)):
-                left_idx = 100 * i
-                right_idx = 100 * (i+1)
-                fake_data[left_idx:right_idx,:] = \
-                        self.gen(torch.randn(100, self.nlatent, device=self.device)).cpu().detach().numpy()
+        if num % 100 == 0:
+            for i in range(num // 100):
+                l, r = 100 * i, 100 * (i+1)
+                fake_data[l:r, :] = \
+                    self.gen(torch.randn(100, self.nlatent,
+                             device=self.device)).cpu().detach().numpy()
         else:
             for i in range(num):
-                fake_data[i,:] =  self.gen(torch.randn(1, self.nlatent, device=self.device)).cpu().detach().numpy()
+                fake_data[i, :] = self.gen(torch.randn(
+                    1, self.nlatent, device=self.device)).cpu().detach().numpy()
         # scaler the data back to original range
         fake_data_scaled = self.scaler.inverse_transform(fake_data)
         del fake_data
         return fake_data_scaled
-    
-
 
 
 def eval(model, data):
     dim = data.shape[1]
-    
+
     # generate fake data using Generator
-    fake_data = np.zeros((50000,dim))
+    fake_data = np.zeros((50000, dim))
     for i in range(500):
         left_idx = 100 * i
         right_idx = 100 * (i+1)
         with torch.no_grad():
-            fake_batch = model.gen(torch.randn(100, model.nlatent, device=model.device)).cpu().detach().numpy()
-        fake_data[left_idx:right_idx,:] = fake_batch
-    # compare mean 
-    real_avg = np.mean(data,axis=0)
-    fake_avg = np.mean(fake_data,axis=0)
+            fake_batch = model.gen(torch.randn(
+                100, model.nlatent, device=model.device)).cpu().detach().numpy()
+        fake_data[left_idx:right_idx, :] = fake_batch
+    # compare mean
+    real_avg = np.mean(data, axis=0)
+    fake_avg = np.mean(fake_data, axis=0)
     avg_diff_pixel = np.sum(np.absolute(real_avg-fake_avg))/dim
     # compare std
-    real_std = np.std(data,axis=0)
-    fake_std = np.std(fake_data,axis=0)
+    real_std = np.std(data, axis=0)
+    fake_std = np.std(fake_data, axis=0)
     std_diff_pixel = np.sum(np.absolute(real_std-fake_std))/dim
     # calculate EMD distance
     distance = np.zeros(dim)
     for i in range(dim):
-        distance[i] = EMD(data[:,i],fake_data[:,i])
+        distance[i] = EMD(data[:, i], fake_data[:, i])
     emd_dist_pixel = np.sum(distance)/dim
-    
-    return avg_diff_pixel,std_diff_pixel,emd_dist_pixel
-    
-    
-    
+
+    return avg_diff_pixel, std_diff_pixel, emd_dist_pixel
+
 
 def init_weights(m):
     if type(m) == nn.Linear:
--- Prj_GAN/SCRIPTS/process_data.py
+++ Prj_GAN/SCRIPTS/process_data.py
@@ -7,6 +7,7 @@ class Africa_Whole_Flat(Dataset):
     """
     Custom Data set for pytorch loading
     """
+
     def __init__(self, data):
         self.data = data
 
@@ -24,7 +25,7 @@ class MinMaxScaler():
     A data scaler that scale data to given range and scale back to initial range
     """
 
-    def __init__(self, range=(-1,1)):
+    def __init__(self, range: tuple[int] = (-1, 1)):
         self.min = np.empty(0)
         self.max = np.empty(0)
         self.diff = np.empty(0)
@@ -46,13 +47,12 @@ class MinMaxScaler():
         :param range: default to (-1,1)
         :return: data scaled to the range of input or (-1,1) by default
         """
-        range_min = self.range[0]
-        range_max = self.range[1]
+        range_min, range_max = self.range
         data_std = (data - self.min) / self.diff
         data_scaled = data_std * (range_max - range_min) + range_min
         return data_scaled
 
-    def inverse_transform(self, data_scaled: np.ndarray):
+    def inverse_transform(self, data_scaled: np.ndarray) -> np.ndarray:
         """
         Transform the scaled data back to original data range
         :param data_scaled: np.ndarray, expected arrangement: row as instances of data and column as features of data
@@ -65,27 +65,25 @@ class MinMaxScaler():
         return data
 
 
-def slice_dataset(data: np.ndarray, slice_ratio = None):
+def slice_dataset(data: np.ndarray, slice_ratio=None):
     """
     partition data at different ratio cutoff, e.g. first 20%, first 40%...
     """
     if slice_ratio is None:
-        slice_ratio = [0.2,0.4,0.6,0.8,1]
-    length,dim = data.shape
+        slice_ratio = [0.2, 0.4, 0.6, 0.8, 1]
+    length, dim = data.shape
     data_slices = []
     for ratio in slice_ratio:
         end_point = int(np.floor(ratio*length))
-        data_slices.append(data[:end_point,:])
+        data_slices.append(data[:end_point, :])
     return data_slices
 
+
 if __name__ == "__main__":
-    
+
     data = np.genfromtxt("./data/R30P.csv", delimiter=',', skip_header=True)
     slices = slice_dataset(data)
-    ratio = [0.2,0.4,0.6,0.8,1]
-    for idx,slice in enumerate(slices):
+    ratio = [0.2, 0.4, 0.6, 0.8, 1]
+    for idx, slice in enumerate(slices):
         file_name = "./data/slices/R30P_{:1.0f%}.csv".format(ratio[idx])
-        np.savetxt(file_name,slice,delimiter=',')
-        
-    
-    
+        np.savetxt(file_name, slice, delimiter=',')
